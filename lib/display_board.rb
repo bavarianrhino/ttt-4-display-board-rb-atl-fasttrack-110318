364fd54683b69c5c6381601b23f9db42362921ae
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-11-03 19:04:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board (board)
-puts row1 = [" ", "board[0]", " ", "|"," ", "board[1]", " ", "|"," ", "board[2]", " "]
+puts row1 = " " + "board[0]" + " " + "|" + " " + "board[1]" + " + "|" + " " + "board[2]" + " "
 puts linerow1 = "-------"
 puts row1 = [" ", "board[3]", " ", "|"," ", "board[4]", " ", "|"," ", "board[5]", " "]
 puts linerow1 = "-------"
